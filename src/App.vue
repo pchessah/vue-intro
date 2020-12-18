bf6efaa2195d19858061797361e98abb3d856ab2
Paul Chesa <paulchesa1@gmail.com>
2020-12-18 16:50:25 +0300

    created employee table component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,52 @@
 <template>
-<div>
+<div id="app" class="small-container">
   
-  <Welcome/>
+ <h1>Employee Table</h1>
+ <employee-table :employees="employeees"></employee-table>>
 </div>
   
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Welcome from "./components/Welcome"
+import EmployeeTable from "@/components/EmployeeTable.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Welcome
+   EmployeeTable
+  },
+  data(){
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .small-container {
+    max-width: 680px;
+  }
 </style>
